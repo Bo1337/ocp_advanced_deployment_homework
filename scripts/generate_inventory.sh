518086a93df8d33cb6cac9a35a49b1014d3b64e7
Ilja Dmitrijevs <idmitrij@redhat.com>
2018-12-06 23:47:27 +0000

    Total commit

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 echo "Copy a new hosts file to /etc/ansible/hosts"
-cp /root/ocp_advanced_deployment_homework/hosts /etc/ansible/hosts
+cp ./hosts /etc/ansible/hosts
+
+# exports GUID line to .bashrc only once even if run multiple times
+if ! grep --quiet GUID /$HOME/.bashrc; then 
+   export GUID=`hostname | cut -d"." -f2`; echo "export GUID=$GUID" >> $HOME/.bashrc
+fi
+
+source $HOME/.bashrc
 
 echo "Set the current GUID to generate the inventory"
-GUID=`hostname|awk -F. '{print $2}'`
-sed -i "s/GUID/$GUID/g" /etc/ansible/hosts
+#sed -i "s/GUID/$GUID/g" /etc/ansible/hosts
+mv /etc/ansible/hosts /etc/ansible/hosts.bak; sed 's/GUID/'$GUID'/g' /etc/ansible/hosts.bak > /etc/ansible/hosts
+rm -rf /etc/ansible/hosts.bak
+ansible all -m shell -a 'export GUID=`hostname | cut -d"." -f2`; echo "export GUID=$GUID" >> $HOME/.bashrc'
